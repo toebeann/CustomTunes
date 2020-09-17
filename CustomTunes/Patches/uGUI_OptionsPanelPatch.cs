5b47702e16f44ae7320238b7f17cf6e1cdc50658
Tobey Blaber <tobey@straitjacket.software>
2020-09-17 01:45:51 +0100

    Reorganised Harmony patches and improved initialisation logging

---
+++ CustomTunes/Patches/uGUI_OptionsPanelPatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using UnityEngine.SceneManagement;
+
+namespace Straitjacket.Subnautica.Mods.CustomTunes.Patches
+{
+    internal static class uGUI_OptionsPanelPatch
+    {
+        #region uGUI_OptionsPanel.OnMasterVolumeChanged
+        [HarmonyPatch(typeof(uGUI_OptionsPanel), nameof(uGUI_OptionsPanel.OnMasterVolumeChanged))]
+        [HarmonyPostfix]
+        static void OnMasterVolumeChangedPostfix()
+        {
+            if (SceneManager.GetActiveScene().name == "Main")
+            {
+                CustomTunes.Main.CalculateVolume();
+            }
+        }
+        #endregion
+
+        #region uGUI_OptionsPanel.OnMusicVolumeChanged
+        [HarmonyPatch(typeof(uGUI_OptionsPanel), nameof(uGUI_OptionsPanel.OnMusicVolumeChanged))]
+        [HarmonyPostfix]
+        static void OnMusicVolumeChangedPostfix()
+        {
+            if (SceneManager.GetActiveScene().name == "Main")
+            {
+                CustomTunes.Main.CalculateVolume();
+            }
+        }
+        #endregion
+    }
+}
